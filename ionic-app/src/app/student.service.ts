13d935ae4d000a7e9bd70cab61a39a078ad10414
VarunGonsalves <gonsalvesvarun@gmail.com>
2024-01-31 15:32:41 +0530

    fouth commit

--- ionic-app/src/app/student.service.ts
+++ ionic-app/src/app/student.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ErrorHandler } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +13,43 @@ export class StudentService {
     this.apiBaseUrl = 'http://127.0.0.1:3000/';
   }
 
-  getStudent(id: string) {
-
+  getStudent(USN: string) {
+    return new Promise(resolve => {
+      this.http.get(this.apiBaseUrl+'students/'+USN).subscribe(data => {
+        resolve(data);
+      })
+    });
   }
 
   getStudents() {
-
+    return new Promise(resolve => {
+      this.http.get(this.apiBaseUrl+'students/').subscribe(data => {
+        resolve(data);
+      })
+    });
   }
 
   addStudent(student: object) {
-
+    return new Promise(resolve => {
+      this.http.post(this.apiBaseUrl+'students/', student).subscribe(data => {
+        resolve(data);
+      })
+    });
   }
 
-  updateStudent(id: string, student: object) {
-
+  updateStudent(USN: string, student: object) {
+    return new Promise(resolve => {
+      this.http.put(this.apiBaseUrl+'students/'+USN, student).subscribe(data => {
+        resolve(data);
+      })
+    });
   }
 
-  deleteStudent(id: string) {
-
+  deleteStudent(USN: string) {
+    return new Promise(resolve => {
+      this.http.delete(this.apiBaseUrl+'students/'+USN).subscribe(data => {
+        resolve(data);
+      })
+    });
   }
 }
